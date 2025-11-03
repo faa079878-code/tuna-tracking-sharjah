ad864d8f2b2aff36d474d1079df0e0f59067f41a
faa079878-code <faa079878@gmail.com>
2025-11-03 09:15:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ legend_html = """
      background-color: rgba(255, 255, 255, 0.85);
      border:2px solid grey; z-index:9999; font-size:14px;
      border-radius: 8px; padding: 10px; color: black;">
-     <b>Fish Ecotype Legend</b><br>
+     <b>Fish Ecotype</b><br>
      <i class="fa fa-fish fa-1x" style="color:blue"></i>&nbsp; Juvenile<br>
      <i class="fa fa-fish fa-1x" style="color:green"></i>&nbsp; Migratory<br>
      <i class="fa fa-fish fa-1x" style="color:red"></i>&nbsp; Resident
